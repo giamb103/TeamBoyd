f741a6a20b7448368bda7feaad2a51a48b281c77
Sierra Clibourne <clibo100@mail.chapman.edu>
2018-09-18 14:16:52 -0700

    started working on interaction stuff

---
+++ Green Thumb/Assets/Scripts/DirtController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Tool" && Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("in if statement");
+            Destroy(gameObject);
+        }
+    }
+
+}
