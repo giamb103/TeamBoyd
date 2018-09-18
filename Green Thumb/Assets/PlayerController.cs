197bef70719f308a9895c9fe05bbb69c777035bc
Sierra Clibourne <clibo100@mail.chapman.edu>
2018-09-18 13:43:54 -0700

    finished player controller (unless people want it to work differently but idk)

---
+++ Green Thumb/Assets/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        //var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3f;
+        var y = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3f;
+
+        transform.Rotate(0, y, 0);
+        transform.Translate(0, 0, z);
+    }
+}
