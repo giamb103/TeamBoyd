0a95acbd5b6aeeabd5da692d553b32f06c5a6dbe
Sierra Clibourne <clibo100@mail.chapman.edu>
2018-09-20 15:38:27 -0700

    plant and timer mechanic

--- Green Thumb/Assets/Scripts/DirtController.cs
+++ Green Thumb/Assets/Scripts/DirtController.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class DirtController : MonoBehaviour {
 
-    public GameObject smallPlant;
+    public GameObject seed;
+    public GameObject timer; 
 
     private bool hasBeenSeeded = false;
 
-	// Use this for initialization
-	void Start () {
+    private GameObject plantObject;
+    private GameObject timerObject;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        if (timerObject != null)
+        {
+            if (timerObject.GetComponent<Timer>().timer < 0f)
+            {
+                Destroy(plantObject);
+                Destroy(timerObject);
+                hasBeenSeeded = false;
+            }
+        }
 	}
 
     private void OnCollisionStay(Collision collision)
@@ -24,7 +37,8 @@ public class DirtController : MonoBehaviour {
         if (collision.gameObject.tag == "Tool" && Input.GetKeyDown(KeyCode.Return) && !hasBeenSeeded)
         {
             //Destroy(gameObject);
-            Instantiate(smallPlant, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
+            plantObject = Instantiate(seed, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
+            timerObject = Instantiate(timer, new Vector3(gameObject.transform.position.x - 0.5f, gameObject.transform.position.y + 1, gameObject.transform.position.z), Quaternion.identity);
             hasBeenSeeded = true;
         }
     }
---
+++ Green Thumb/Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public float waitTime = 30f;
+
+    public GameObject timerText;
+
+    public float timer;
+
+    private void Start()
+    {
+        timer = waitTime;    
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        timerText.GetComponent<TextMesh>().text = Mathf.Round(timer).ToString();
+
+        if (timer < 5.1)
+        {
+            timerText.GetComponent<TextMesh>().color = Color.red;
+        }
+    }
+}
