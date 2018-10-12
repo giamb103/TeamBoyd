492495ac909844e14921e250ed5f5273e870b2ff
green327 <green327@mail.chapman.edu>
2018-10-12 12:29:06 -0700

    shop screen scene

---
+++ Green Thumb/Assets/Scripts/Nightime/Nighttime.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Nighttime : MonoBehaviour {
+    public Button shop;
+    public Button nextDay;
+    public Button menu;
+    public Button settings;
+
+    public Canvas Shop;
+    public Canvas Settings;
+    public Canvas MainMenu;
+
+    // Use this for initialization
+    void Start ()
+    {
+        shop.onClick.AddListener(onClickShop);
+        nextDay.onClick.AddListener(onClicknextDay);
+        menu.onClick.AddListener(onClickMenu);
+        settings.onClick.AddListener(onClickSettings);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+
+    void onClicknextDay()
+    {
+        
+    }
+
+    void onClickShop()
+    {
+        Shop.enabled = true;
+    }
+
+    void onClickMenu()
+    {
+        MainMenu.enabled = true;
+    }
+
+    void onClickSettings()
+    {
+        Settings.enabled = true;
+    }
+}
